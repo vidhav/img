6e2a50ca4b29af7502cfbf7aedea4b378edbf19b
Håvard Vidme <havard.vidme@gmail.com>
2011-12-09 10:20:46 +0100

    Corrected tree structure

--- core/components/img/elements/snippets/thumb.snippet.php
+++
@@ -1,9 +0,0 @@
-<?php
-$src = '';
-$img_start = strpos($input, '<img ', 0);
-if ($img_start !== false) {
-    $src_start = strpos($input, ' src="', $img_start) + 6;
-    $src_length = strpos($input, '"', $src_start) - $src_start;
-    $src = substr($input, $src_start, $src_length);
-}
-return empty($src) ? 'asset/images/dummy.jpg' : $src;
